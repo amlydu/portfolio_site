11bf0182755e153081ef0a0daf0bfcf0a0e3b344
Amy Dusek <amlydu@umich.edu>
2015-01-29 14:02:47 -0500

    Added remaining photo and changed the styles to incorporate the photo- this included deleting some media queries. Modified some of the images in my timeline to make them cleaner looking within the bubbles

--- js/scripts.js
+++ js/scripts.js
@@ -167,25 +167,24 @@ $(window).load(function() {
 /***************** Quote Slideshow ******************/
 
 $(document).ready(function(){
- 
-var counter = 0,
-$items = $('.quote-slideshow figure'),
-numItems = $items.length;
- 
-var showCurrent = function(){
-var itemToShow = Math.abs(counter%numItems);
-$items.removeClass('show');
-$items.eq(itemToShow).addClass('show');
-};
- 
-$('.quote-next').on('click', function(){
-counter++;
-showCurrent();
-});
- 
-$('.quote-prev').on('click', function(){
-counter--;
-showCurrent();
-});
+	var counter = 0,
+	$items = $('.quote-slideshow figure'),
+	numItems = $items.length;
+	 
+	var showCurrent = function(){
+		var itemToShow = Math.abs(counter%numItems);
+		$items.removeClass('show');
+		$items.eq(vaitemToShow).addClass('show');
+	};
+	 
+	$('.quote-next').on('click', function(){
+		counter++;
+		showCurrent();
+	});
+	 
+	$('.quote-prev').on('click', function(){
+		counter--;
+		showCurrent();
+	});
  
 });
